0e382027643943d652156eaa4592478815da2f91
FelixAngelMartinez <32263279+FelixAngelMartinez@users.noreply.github.com>
2017-10-08 21:10:38 +0200

    Update Main.java

--- tractor/src/tractor/Main.java
+++ tractor/src/tractor/Main.java
@@ -3,6 +3,7 @@ package tractor;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
 
@@ -14,14 +15,14 @@ public class Main {
 		int peticion = 0;
 		Scanner TECLADO = new Scanner(System.in);
 		try {
-		System.out.println(
-				"¿Desea meter los valores por archivo de texto? En caso negativo los datos se usaran valores aleatorios \n 1-Si \n 2-No ");
-		peticion = TECLADO.nextInt();
-		if (peticion < 1 || peticion > 2) {
-			System.out.println("Opcion incorrecta, introduzca de nuevo");
+			System.out.println(
+					"Â¿Desea meter los valores por archivo de texto? En caso negativo los datos se usaran valores aleatorios \n 1-Si \n 2-No ");
 			peticion = TECLADO.nextInt();
-		}
-		}catch(Exception e) {
+			if (peticion < 1 || peticion > 2) {
+				System.out.println("Opcion incorrecta, introduzca de nuevo");
+				peticion = TECLADO.nextInt();
+			}
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			OpcionEjecucion();
 		}
@@ -29,7 +30,9 @@ public class Main {
 	}
 
 	public static void Menu() {
-		String ruta="D:/3º/inteligentes/inteligentesJava/tractor/";
+		BuscarArchivo();
+		String ruta="C:/Users/Martinez/OneDrive - Universidad de Castilla-La Mancha/1_Entorno_Desarrollo/Tractor/";
+		System.out.println("Test, la ruta actual es"+ruta);
 		Terrenoss t = new Terrenoss();
 		int peticion = OpcionEjecucion();
 		Scanner TECLADO = new Scanner(System.in);
@@ -37,13 +40,13 @@ public class Main {
 		case 1:
 			try {
 				System.out.println("Introduzca la ruta deseada \n");
-				//String ruta = TECLADO.next();				
+				// String ruta = TECLADO.next();
 				System.out.println(ruta);
 				BorrarContenidoFichero(ruta);
 				t.TerrenoFichero(ruta);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
-				
+
 			}
 			break;
 		case 2:
@@ -52,14 +55,53 @@ public class Main {
 			break;
 		}
 	}
+
 	public static void BorrarContenidoFichero(String Archivo) {
 		try {
-			FileWriter  fos = new FileWriter (Archivo + "TerrenosFinales.txt");
+			FileWriter fos = new FileWriter(Archivo + "TerrenosFinales.txt");
 			BufferedWriter bw = new BufferedWriter(fos);
 			bw.write("");
 			bw.close();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	public static String BuscarArchivo() {
+		File f = null;
+		String[] strs = { "TerrenoInicial.txt", "TerrenosFinales.txt" };
+		String ruta=null;
+		String directorio=null;
+		boolean existir;
+		try {
+			// for each string in string array
+			for (String s : strs) {
+				// create new file
+				f = new File(s);
+				
+				// Prueba la existencia
+				existir=f.exists();
+				System.out.println("El archivo existe: "+existir);
+				
+				// true if the file is executable
+				boolean bool = f.canExecute();
+
+				// find the absolute path
+				ruta = f.getAbsolutePath();
+	
+				// prints absolute path
+				System.out.print(ruta);
+
+				// prints
+				System.out.println(" is executable: " + bool);
+				
+				directorio=f.getParent();
+			}			
+		}
+		catch (Exception e) {
+			// if any I/O error occurs
+			e.printStackTrace();
+		}
+		return directorio;
 	}
 }
