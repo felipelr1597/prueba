02737896c03b15e86ccdd636c3c5669a9412d68f
Felipe <Felipe@172.19.250.89>
2017-10-11 13:34:50 +0200

    hola borras

--- tractor/src/tractor/Main.java
+++ tractor/src/tractor/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.File;
-
+// hola borras
 public class Main {
 
 	public static void main(String[] args) {
